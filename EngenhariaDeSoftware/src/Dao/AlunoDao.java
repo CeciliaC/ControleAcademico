65d584478fdf72c195be2e464f1e95b16a75c148
Breno Negreiros <breno-negreiros@hotmail.com>
2017-11-01 12:08:18 -0200

    adicionando junit aluno

--- EngenhariaDeSoftware/src/Dao/AlunoDao.java
+++ EngenhariaDeSoftware/src/Dao/AlunoDao.java
@@ -89,4 +89,6 @@ public class AlunoDao {
 	        stmt.close();
 	    }
 
+		
+
 }
--- EngenhariaDeSoftware/src/JunitClassesTestes/JunitAluno.java
+++ EngenhariaDeSoftware/src/JunitClassesTestes/JunitAluno.java
@@ -2,8 +2,11 @@ package JunitClassesTestes;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 
+import Dao.AlunoDao;
 import Model.Aluno;
 
 public class JunitAluno {
@@ -11,6 +14,17 @@ public class JunitAluno {
 	public void TestandoAluno() {
 		Aluno aluno = new Aluno();
 		aluno.setNome("fulano");
+		aluno.setCpf(123);
+		
 		assertEquals("fulano",aluno.getNome());
+		assertEquals(123,aluno.getCpf());
 	}
+	
+	@Test
+	public void TesteListandoAluno() throws SQLException {
+		AlunoDao alunodao = new AlunoDao();
+		assertEquals("Listando com sucesso!",2,alunodao.listar().size());
+		
+	}
+
 }
